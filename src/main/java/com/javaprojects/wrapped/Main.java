06f9c07fa5a619520f1b0bccba507d8ab396de6c
ahmadclyburn <ahmadclyburn61@gmail.com>
2024-11-27 15:40:54 -0500

    manipulated csv data

--- src/main/java/com/javaprojects/wrapped/Main.java
+++ src/main/java/com/javaprojects/wrapped/Main.java
@@ -1,7 +1,35 @@
 package com.javaprojects.wrapped;
 
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String prompt = "Your History";
+        System.out.println(prompt);
+//        String[] splitPrompt = prompt.split("z");
+//        System.out.println("aftersplit: " + Arrays.toString(splitPrompt));
+        try{
+        FileReader reader = new FileReader("src/main/resources/listening_history.csv");
+        BufferedReader bufReader = new BufferedReader(reader);
+        String currentLine;
+        while( (currentLine = bufReader.readLine()) != null){
+            String [] songInfo = currentLine.split("[|]");
+            System.out.println(songInfo[2]);
+            String songName = songInfo[0];
+            String artistName = songInfo[1];
+            String genre = songInfo[2];
+//            System.out.println(Arrays.toString(songInfo));
+//            System.out.println(artistName);
+//            System.out.println(genre + ", " + songName);
+//            hip hop gnx
+        }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
